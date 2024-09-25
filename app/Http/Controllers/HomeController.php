e8305b4fe35829ab7b9bf4cd712a6a533f3dd35a
mahdi zangeneh <67897876+bigmemti@users.noreply.github.com>
2024-09-25 23:17:09 +0330

    remove some variables

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Inertia\Inertia;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 class HomeController extends Controller
 {
@@ -13,9 +11,7 @@ class HomeController extends Controller
         $posts = Post::latest()->limit(4)->get()->load(['category', 'user'])->loadCount('comments');
 
         return Inertia::render('Home/Welcome', [
-            'posts' => $posts,
-            'canLogin' => Route::has('login'),
-            'canRegister' => Route::has('register'),
+            'posts' => $posts
         ]);
     }
 }
